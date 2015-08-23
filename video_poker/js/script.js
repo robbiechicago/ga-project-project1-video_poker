a721e3865037d39ef7861b70d4ff293bc370c50f
Rob Holloway <robbiechicago@gmail.com>
2015-08-23 14:21:18 +0100

    deck array created.  Seems to be OK

--- video_poker/js/script.js
+++ video_poker/js/script.js
@@ -1,29 +1,28 @@
 $(document).ready(function(){
   console.log("Ready to go, RoHo");
-  var vp = {}
 
-  var cardFactory = function(number, suit, value) {
-    number: number;
-    suit: suit;
-    value; value;
+function card(value, number, suit){
+  this.value = value;
+  this.number = number;
+  this.suit = suit;
+}
+
+function deck() {
+  this.numbers = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+  this.suits = ['D','H','S','C'];
+  var cards = [];
+    
+  for(var i = 0, iLen = this.suits.length; i < iLen; i++) {
+    for(var j = 0, jLen = this.numbers.length; j < jLen; j++) {
+      cards.push(new card(j+2, this.numbers[j], this.suits[i] ) );
+    }
   }
+  return cards;
+}
 
-  var vp.numbers = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
-  var vp.suits = ['D','S','H','C'];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var newDeck = new deck();
+console.log(newDeck);
+console.log(newDeck[25]);
 
 
 
