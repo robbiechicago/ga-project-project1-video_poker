a120c8ddbaa82c066352645807a465072b3e4960
Rob Holloway <robbiechicago@gmail.com>
2015-08-23 12:10:50 +0100

    Initial commit.  Set up basic html, css and js pages, tested and verified connections, started creating deck of cards object

---
+++ video_poker/js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+  console.log("Ready to go, RoHo");
+  var vp = {}
+
+  var cardFactory = function(number, suit, value) {
+    number: number;
+    suit: suit;
+    value; value;
+  }
+
+  var vp.numbers = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+  var vp.suits = ['D','S','H','C'];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
