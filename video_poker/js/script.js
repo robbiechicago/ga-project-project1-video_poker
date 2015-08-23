bfbcd2c40eaf9fd64d99fb3ded7babc675a9a20b
Rob Holloway <robbiechicago@gmail.com>
2015-08-23 16:52:20 +0100

    ten random cards now spliced from the deck array and into the tenRandomCards array.  The first five of these will be used in the initial deal, the remaining five are reserved for any changes the player wants to make

--- video_poker/js/script.js
+++ video_poker/js/script.js
@@ -1,28 +1,57 @@
 $(document).ready(function(){
   console.log("Ready to go, RoHo");
 
-function card(value, number, suit){
+//constructor function to make the individual cards
+function card(value, number, suit, name){
   this.value = value;
   this.number = number;
   this.suit = suit;
+  this.name = name;
 }
 
-function deck() {
+//create a deck array using the card constructor
+function deckArray() {
   this.numbers = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
   this.suits = ['D','H','S','C'];
   var cards = [];
     
   for(var i = 0, iLen = this.suits.length; i < iLen; i++) {
     for(var j = 0, jLen = this.numbers.length; j < jLen; j++) {
-      cards.push(new card(j+2, this.numbers[j], this.suits[i] ) );
+      cards.push(new card(j+2, this.numbers[j], this.suits[i], this.numbers[j] + this.suits[i]));
     }
   }
   return cards;
 }
 
-var newDeck = new deck();
-console.log(newDeck);
-console.log(newDeck[25]);
+//assign a deck to a variable deck
+var deck = deckArray();
+var randTen = [];
+var aRandTen = tenRandomCards();
+
+
+console.log(deck[25]);
+
+//create five random cards
+//THIS IS NOT RIGHT!  NEED TO USE ARRAYS AND ARRAY LENGTH TO ASSURE NO DUPLICATION, YO
+// var card1 = Math.floor(Math.random()*52)
+// var card2 = Math.floor(Math.random()*52)
+// var card3 = Math.floor(Math.random()*52)
+// var card4 = Math.floor(Math.random()*52)
+// var card5 = Math.floor(Math.random()*52)
+
+function tenRandomCards() {
+  for (var i = 0; i < 10; i++) {
+    var randIndex = Math.floor(Math.random()*deck.length);
+    var randCard = deck[randIndex];
+    console.log(randCard);
+    deck.splice(randIndex,1);
+    randTen.push(randCard);
+  }
+}
+
+console.log(deck);
+console.log(randTen);
+
 
 
 
